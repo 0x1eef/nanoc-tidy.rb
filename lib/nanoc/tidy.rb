19e7fe131df470e06b57a8638e7164c30587ef49
0x1eef <0x1eef@protonmail.com>
2024-02-25 12:38:24 -0300

    Add Nanoc::Tidy.default_options

--- lib/nanoc/tidy.rb
+++ lib/nanoc/tidy.rb
@@ -4,4 +4,11 @@ require "nanoc"
 module Nanoc::Tidy
   require_relative "tidy/version"
   require_relative "tidy/filter"
+
+  ##
+  # @example (see Nanoc::Tidy::Filter.default_options)
+  # @return (see Nanoc::Tidy::Filter.default_options)
+  def self.default_options
+    Filter.default_options
+  end
 end
