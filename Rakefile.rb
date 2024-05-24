0c4397729f403131a63de987e84459eb8c458bf9
0x1eef <0x1eef@protonmail.com>
2024-05-24 18:11:07 -0300

    Add 'rake ci'

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,5 +1,11 @@
 require "rake/testtask"
 
+desc "Run CI tasks"
+task :ci do
+  sh "rake test"
+  sh "bundle exec rubocop"
+end
+
 Rake::TestTask.new do |t|
   t.test_files = FileList['test/*_test.rb']
   t.verbose = true
